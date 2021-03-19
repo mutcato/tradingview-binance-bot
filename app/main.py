a54c89d47795358d673fc0b25cd2ec78c265f2f3
Mutlucan Tokat <6348274-mutlucan@users.noreply.gitlab.com>
2021-03-19 23:25:35 +0300

    First commit

---
+++ app/main.py
@@ -0,0 +1,12 @@
+# app.py
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return {'hello': 'world'}
+
+if __name__ == '__main__':
+    uvicorn.run(app)
